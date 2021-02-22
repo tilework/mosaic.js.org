8314283e8646753f2fc18dffdf004e7ea0e9d755
Alfreds Genkins <alfreds@scandiweb.com>
2021-02-22 18:58:51 +0400

    Added playground

--- src/component/Playground/Playground.component.js
+++ src/component/Playground/Playground.component.js
@@ -8,6 +8,12 @@ const Playground = () => (
     <section className="Playground">
         <ContentWrapper className="Playground-Wrapper">
             <Heading type="h2" className="Playground-Heading">Give it a try</Heading>
+            <iframe
+                src="https://codesandbox.io/embed/wip-forked-y7z9y?fontsize=14&hidenavigation=1&theme=dark"
+                title="WIP  (forked)"
+                allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+                sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+            />
         </ContentWrapper>
     </section>
 );
