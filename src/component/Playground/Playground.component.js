b9f311fb0a07b901862476547b6ff2c8f937ac5d
Alfreds Genkins <alfreds@scandiweb.com>
2021-02-23 13:28:28 +0400

    Added icons as background

--- src/component/Playground/Playground.component.js
+++ src/component/Playground/Playground.component.js
@@ -8,9 +8,8 @@ const Playground = () => (
     <section className="Playground">
         <ContentWrapper className="Playground-Wrapper">
             <Heading type="h2" className="Playground-Heading">Give it a try</Heading>
-            <iframe
-                src="https://codesandbox.io/embed/wip-forked-y7z9y?fontsize=14&hidenavigation=1&theme=dark"
-                title="WIP  (forked)"
+            <iframe src="https://codesandbox.io/embed/plugjs-playground-y7z9y?fontsize=14&hidenavigation=1&module=%2Fsrc%2FApp.js&theme=dark"
+                title="Plug.js playground"
                 allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
                 sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
             />
