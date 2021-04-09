cf25facd29ea0a73b9bfc73240065b09766bd54b
yeegor <yegor.batov@gmail.com>
2021-04-09 14:15:58 +0300

    Updates

--- src/component/ContentWrapper/ContentWrapper.component.js
+++
@@ -1,9 +0,0 @@
-import './ContentWrapper.style.scss';
-
-const ContentWrapper = ({ children, className, ...props }) => (
-    <div className={ `ContentWrapper ${ className }` } { ...props }>
-        { children }
-    </div>
-);
-
-export default ContentWrapper;
